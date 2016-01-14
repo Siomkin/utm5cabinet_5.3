fa5f02173862c9a67b06d9499187b331487353c8
Siomkin Alexander <darang@tut.by>
2016-01-14 11:07:50 +0300

    dhs report fix

--- library/Urfa/Client.php
+++ library/Urfa/Client.php
@@ -1241,7 +1241,8 @@ class Urfa_Client
     {
         $report = array();
 
-        $this->urfa->call(-16407);
+        //$this->urfa->call(-16407);
+        $this->urfa->call(-16464);
         $this->urfa->put_int((int)$startDay);
         $this->urfa->put_int((int)$endDay);
 
@@ -1259,8 +1260,6 @@ class Urfa_Client
             // $tmp['framed_ip'] = Urfa_Resolve::ip2string($this->urfa->get_int());
             $tmp['framed_ip'] = $this->urfa->get_ip_address()->toString();
             $tmp['framed_ip6'] = $this->urfa->get_ip_address()->toString();
-
-
             $tmp['nas_port'] = $this->urfa->get_int();
             $tmp['session_id'] = $this->urfa->get_string();
             $tmp['nas_port_type'] = $this->urfa->get_int();
@@ -1275,6 +1274,11 @@ class Urfa_Client
             $tmp['input_bytes'] = $this->urfa->get_long();
             $tmp['output_pack'] = $this->urfa->get_long();
             $tmp['output_bytes'] = $this->urfa->get_long();
+
+            $tmp['time'] = $this->urfa->get_long();
+            $tmp['calling_station_id'] = $this->urfa->get_string();
+            $tmp['called_station_id'] = $this->urfa->get_string();
+
             $tmp['session_time'] = Urfa_Resolve::getTimeFromSec($this->urfa->get_long());
 
             $cost = 0.0;
